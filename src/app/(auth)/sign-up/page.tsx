0c634486e036b0bcefed6506009f953ea234a737
Deepak Varshney <deepakvarshney.com@gmail.com>
2026-05-19 13:38:41 +0530

    authentication ui

---
+++ src/app/(auth)/sign-up/page.tsx
@@ -0,0 +1,10 @@
+import React from 'react'
+import { Card, CardContent, CardDescription, CardHeader } from '@/components/ui/card'
+import { SignUpView } from '@/modules/auth/ui/views/sign-up-view'
+
+
+const Signup = () => {
+    return (<SignUpView />)
+}
+
+export default Signup
