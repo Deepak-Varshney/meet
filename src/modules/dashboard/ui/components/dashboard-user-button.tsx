773b80f63b0413966ffeb05e53fb3d89c72c65c4
Deepak Varshney <deepakvarshney.com@gmail.com>
2026-05-20 15:26:50 +0530

    fix mobile responsiveness and added navbar

--- src/modules/dashboard/ui/components/dashboard-user-button.tsx
+++ src/modules/dashboard/ui/components/dashboard-user-button.tsx
@@ -1,44 +1,108 @@
+"use client";
 
-import { authClient } from "@/lib/auth-client"
+import { authClient } from "@/lib/auth-client";
 
-import { Button } from "@/components/ui/button"
+import { Button } from "@/components/ui/button";
 import {
     DropdownMenu,
     DropdownMenuContent,
     DropdownMenuGroup,
     DropdownMenuItem,
     DropdownMenuLabel,
-    DropdownMenuSeparator,
     DropdownMenuTrigger,
-} from "@/components/ui/dropdown-menu"
+} from "@/components/ui/dropdown-menu";
+
+import { Drawer, DrawerContent, DrawerDescription, DrawerTrigger, DrawerFooter, DrawerHeader, DrawerTitle } from "@/components/ui/drawer";
+
 import {
     Avatar,
-    AvatarBadge,
     AvatarFallback,
-    AvatarGroup,
-    AvatarGroupCount,
     AvatarImage,
-} from "@/components/ui/avatar"
-import { useRouter } from "next/navigation"
-import { LogOutIcon } from "lucide-react"
+} from "@/components/ui/avatar";
 
-
-
-
+import { useRouter } from "next/navigation";
+import { ChevronDownIcon, CreditCardIcon, LogOutIcon } from "lucide-react";
+import { useIsMobile } from "@/hooks/use-mobile";
 
 export const DashboardUserButton = () => {
+    const router = useRouter();
+    const isMobile = useIsMobile();
+
     const { data, isPending } = authClient.useSession();
     if (isPending || !data?.user) {
         return null;
     }
-    const router = useRouter();
-    const initials =
-        data.user.name
-            ?.split(" ")
-            .map((part) => part[0])
-            .join("")
-            .slice(0, 2)
-            .toUpperCase() || "U"
+    const initials = data.user.name
+        ?.split(" ")
+        .map((part) => part[0])
+        .join("")
+        .slice(0, 2)
+        .toUpperCase() || "U";
+    if (isMobile) {
+        return (
+            <Drawer>
+                <DrawerTrigger asChild
+                    className="w-full"
+                >
+                    <Button
+                        variant="ghost"
+                        className="w-full justify-start h-auto p-3 rounded-lg"
+                    >
+                        <div className="flex items-center gap-3 w-full overflow-hidden">
+                            <Avatar className="h-10 w-10 shrink-0">
+                                <AvatarImage
+                                    src={data.user.image || ""}
+                                    alt={data.user.name}
+                                />
+                                <AvatarFallback>
+                                    {initials}
+                                </AvatarFallback>
+                            </Avatar>
+
+                            <div className="flex flex-col items-start overflow-hidden">
+                                <span className="truncate text-sm font-medium">
+                                    {data.user.name}
+                                </span>
+
+                                <span className="truncate text-xs text-muted-foreground">
+                                    {data.user.email}
+                                </span>
+
+                            </div>
+                            <ChevronDownIcon className="size-5" />
+                        </div>
+                    </Button>
+                </DrawerTrigger>
+                <DrawerContent>
+                    <DrawerHeader>
+                        <DrawerTitle>{data.user.name}</DrawerTitle>
+                        <DrawerDescription>{data.user.email}</DrawerDescription>
+                    </DrawerHeader>
+                    <DrawerFooter>
+                        <Button variant="destructive" onClick={() =>
+                            authClient.signOut({
+                                fetchOptions: {
+                                    onSuccess: () => {
+                                        router.push("/sign-in");
+                                    },
+                                },
+                            })
+                        }>
+                            Logout<LogOutIcon className="size-4" />
+                        </Button>
+                        <Button variant={"outline"}>
+                            Billing
+                            <CreditCardIcon className="size-4" />
+                        </Button>
+
+                    </DrawerFooter>
+                </DrawerContent>
+            </Drawer >
+        )
+    }
+
+
+
     return (
         <DropdownMenu>
             <DropdownMenuTrigger
@@ -52,10 +116,12 @@ export const DashboardUserButton = () => {
                     <div className="flex items-center gap-3 w-full overflow-hidden">
                         <Avatar className="h-10 w-10 shrink-0">
                             <AvatarImage
-                                src={data?.user?.image || ""}
+                                src={data.user.image || ""}
                                 alt={data.user.name}
                             />
-                            <AvatarFallback>{initials}</AvatarFallback>
+                            <AvatarFallback>
+                                {initials}
+                            </AvatarFallback>
                         </Avatar>
 
                         <div className="flex flex-col items-start overflow-hidden">
@@ -67,22 +133,42 @@ export const DashboardUserButton = () => {
                                 {data.user.email}
                             </span>
                         </div>
+                        <ChevronDownIcon className="size-5" />
+
                     </div>
                 </Button>
             </DropdownMenuTrigger>
-            <DropdownMenuContent align="end" side="right" className="w-44">
+
+            <DropdownMenuContent
+                align="end"
+                side="right"
+                className="w-44"
+            >
                 <DropdownMenuGroup>
-                    <DropdownMenuLabel>My Account</DropdownMenuLabel>
-                    <DropdownMenuItem>Profile</DropdownMenuItem>
-                    <DropdownMenuItem onClick={() => authClient.signOut({
-                        fetchOptions: {
-                            onSuccess: () => router.push("/sign-in")
+                    <DropdownMenuLabel>
+                        My Account
+                    </DropdownMenuLabel>
+
+                    <DropdownMenuItem>
+                        Profile
+                    </DropdownMenuItem>
+
+                    <DropdownMenuItem
+                        onClick={() =>
+                            authClient.signOut({
+                                fetchOptions: {
+                                    onSuccess: () => {
+                                        router.push("/sign-in");
+                                    },
+                                },
+                            })
                         }
-                    })}>Logout
-                    <LogOutIcon className="size-4"/>
+                    >
+                        Logout
+                        <LogOutIcon className="size-4 ml-auto" />
                     </DropdownMenuItem>
                 </DropdownMenuGroup>
             </DropdownMenuContent>
         </DropdownMenu>
-    )
-}
+    );
+};
